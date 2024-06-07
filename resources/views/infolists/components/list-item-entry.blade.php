9bade956ed690dbecf4d115b4d154e80e87471f3
abiisaleh <abi.saleh.25@gmail.com>
2024-06-07 16:14:49 +0900

    fix template

--- resources/views/infolists/components/list-item-entry.blade.php
+++ resources/views/infolists/components/list-item-entry.blade.php
@@ -4,8 +4,9 @@
             @foreach ($getState() as $item)
                 @if ($item['pertanyaan'])
                     <li type="1" style="margin-top: .5rem">{{ $item['pertanyaan'] }}</li>
+                    <img src="{{ '/storage/' . $item['gambar'] }}" alt="{{ $item['gambar'] }}" width="250">
                     @if (isset($item['opsi']))
-                        <ol style="margin-left: 1.2rem;">
+                        <ol style="margin-left: 1.2rem; margin-top: .15rem">
                             @foreach ($item['opsi'] as $opsi)
                                 @if ($opsi)
                                     <li type="a">{{ $opsi }}</li>
